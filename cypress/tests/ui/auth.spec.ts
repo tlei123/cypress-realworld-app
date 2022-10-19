e45b49cc334861b9ef50452bd37e7ecc79b37c7a
Tze-chiu Lei <tclei2009@gmail.com>
2022-10-18 18:04:46 -0700

    Add describe blocks to UI Auth spec.

--- cypress/tests/ui/auth.spec.ts
+++ cypress/tests/ui/auth.spec.ts
@@ -17,17 +17,19 @@ describe("User Sign-up and Login", function () {
     });
   });
 
-  it("should redirect unauthenticated user to signin page - C23984", function () {
-    cy.visit("/personal");
-    cy.location("pathname").should("equal", "/signin");
-    cy.visualSnapshot("Redirect to SignIn");
-  });
-
-  it("should redirect to the home page after login - C23985", function () {
-    cy.database("find", "users").then((user: User) => {
-      cy.login(user.username, "s3cret", { rememberUser: true });
+  describe("redirects", () => {
+    it("should redirect unauthenticated user to signin page - C23984", function () {
+      cy.visit("/personal");
+      cy.location("pathname").should("equal", "/signin");
+      cy.visualSnapshot("Redirect to SignIn");
     });
-    cy.location("pathname").should("equal", "/");
+
+    it("should redirect to the home page after login - C23985", function () {
+      cy.database("find", "users").then((user: User) => {
+        cy.login(user.username, "s3cret", { rememberUser: true });
+      });
+      cy.location("pathname").should("equal", "/");
+    });
   });
 
   it("should remember a user for 30 days after login - C23986", function () {
@@ -108,67 +110,70 @@ describe("User Sign-up and Login", function () {
     cy.visualSnapshot("Redirect to SignIn");
   });
 
-  it("should display login errors - C23988", function () {
-    cy.visit("/");
+  describe("error-states", () => {
+    it("should display login errors - C23988", function () {
+      cy.visit("/");
 
-    cy.getBySel("signin-username").type("User").find("input").clear().blur();
-    cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
-    cy.visualSnapshot("Display Username is Required Error");
+      cy.getBySel("signin-username").type("User").find("input").clear().blur();
+      cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
+      cy.visualSnapshot("Display Username is Required Error");
 
-    cy.getBySel("signin-password").type("abc").find("input").blur();
-    cy.get("#password-helper-text")
-      .should("be.visible")
-      .and("contain", "Password must contain at least 4 characters");
-    cy.visualSnapshot("Display Password Error");
+      cy.getBySel("signin-password").type("abc").find("input").blur();
+      cy.get("#password-helper-text")
+        .should("be.visible")
+        .and("contain", "Password must contain at least 4 characters");
+      cy.visualSnapshot("Display Password Error");
 
-    cy.getBySel("signin-submit").should("be.disabled");
-    cy.visualSnapshot("Sign In Submit Disabled");
-  });
-
-  it("should display signup errors - C23989", function () {
-    cy.intercept("GET", "/signup");
-
-    cy.visit("/signup");
-
-    cy.getBySel("signup-first-name").type("First").find("input").clear().blur();
-    cy.get("#firstName-helper-text").should("be.visible").and("contain", "First Name is required");
-
-    cy.getBySel("signup-last-name").type("Last").find("input").clear().blur();
-    cy.get("#lastName-helper-text").should("be.visible").and("contain", "Last Name is required");
-
-    cy.getBySel("signup-username").type("User").find("input").clear().blur();
-    cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
-
-    cy.getBySel("signup-password").type("password").find("input").clear().blur();
-    cy.get("#password-helper-text").should("be.visible").and("contain", "Enter your password");
-
-    cy.getBySel("signup-confirmPassword").type("DIFFERENT PASSWORD").find("input").blur();
-    cy.get("#confirmPassword-helper-text")
-      .should("be.visible")
-      .and("contain", "Password does not match");
-    cy.visualSnapshot("Display Sign Up Required Errors");
-
-    cy.getBySel("signup-submit").should("be.disabled");
-    cy.visualSnapshot("Sign Up Submit Disabled");
-  });
-
-  it("should error for an invalid user - C23990", function () {
-    cy.login("invalidUserName", "invalidPa$$word");
-
-    cy.getBySel("signin-error")
-      .should("be.visible")
-      .and("have.text", "Username or password is invalid");
-    cy.visualSnapshot("Sign In, Invalid Username and Password, Username or Password is Invalid");
-  });
-
-  it("should error for an invalid password for existing user - C23991", function () {
-    cy.database("find", "users").then((user: User) => {
-      cy.login(user.username, "INVALID");
+      cy.getBySel("signin-submit").should("be.disabled");
+      cy.visualSnapshot("Sign In Submit Disabled");
     });
 
-    cy.getBySel("signin-error")
-      .should("be.visible")
-      .and("have.text", "Username or password is invalid");
-    cy.visualSnapshot("Sign In, Invalid Username, Username or Password is Invalid");
+    it("should display signup errors - C23989", function () {
+      cy.intercept("GET", "/signup");
+
+      cy.visit("/signup");
+
+      cy.getBySel("signup-first-name").type("First").find("input").clear().blur();
+      cy.get("#firstName-helper-text")
+        .should("be.visible")
+        .and("contain", "First Name is required");
+
+      cy.getBySel("signup-last-name").type("Last").find("input").clear().blur();
+      cy.get("#lastName-helper-text").should("be.visible").and("contain", "Last Name is required");
+
+      cy.getBySel("signup-username").type("User").find("input").clear().blur();
+      cy.get("#username-helper-text").should("be.visible").and("contain", "Username is required");
+
+      cy.getBySel("signup-password").type("password").find("input").clear().blur();
+      cy.get("#password-helper-text").should("be.visible").and("contain", "Enter your password");
+
+      cy.getBySel("signup-confirmPassword").type("DIFFERENT PASSWORD").find("input").blur();
+      cy.get("#confirmPassword-helper-text")
+        .should("be.visible")
+        .and("contain", "Password does not match");
+      cy.visualSnapshot("Display Sign Up Required Errors");
+
+      cy.getBySel("signup-submit").should("be.disabled");
+      cy.visualSnapshot("Sign Up Submit Disabled");
+    });
+    it("should error for an invalid user - C23990", function () {
+      cy.login("invalidUserName", "invalidPa$$word");
+
+      cy.getBySel("signin-error")
+        .should("be.visible")
+        .and("have.text", "Username or password is invalid");
+      cy.visualSnapshot("Sign In, Invalid Username and Password, Username or Password is Invalid");
+    });
+
+    it("should error for an invalid password for existing user - C23991", function () {
+      cy.database("find", "users").then((user: User) => {
+        cy.login(user.username, "INVALID");
+      });
+
+      cy.getBySel("signin-error")
+        .should("be.visible")
+        .and("have.text", "Username or password is invalid");
+      cy.visualSnapshot("Sign In, Invalid Username, Username or Password is Invalid");
+    });
   });
 });
